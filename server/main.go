d7bd823afdab14b0d81e2edb5b558ab7a959ecfe
unknown <ilovekr@gmail.com>
2020-02-07 23:56:57 +0900

    pool적용

--- server/main.go
+++ server/main.go
@@ -12,7 +12,6 @@ import (
 	"strings"
 	"sync"
 
-	f "github.com/sjuhan/face_server/face"
 	pb "github.com/sjuhan/face_server/proto"
 
 	"github.com/Kagami/go-face"
@@ -31,37 +30,68 @@ var (
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	*facestruct
-	f *pb.Face
 
-	rec *face.Recognizer
-	m   *sync.Mutex
+	f              *pb.Face
+	m              *sync.Mutex
+	respool        sync.Pool
+	ffpool         sync.Pool
+	facestructpool sync.Pool
+	recpool        sync.Pool
 }
 
 type facestruct struct {
 	samples []face.Descriptor
-	jumins  []string
-	names   []string
 	cats    []int32
+
+	identify map[int32]identifypt
+
+	cat int32
+}
+
+type identifypt struct {
+	jumin string
+	name  string
 }
 
 func newserverstruct() *server {
-	var err error
 	server := new(server)
+
 	server.facestruct = new(facestruct)
-	server.rec, err = face.NewRecognizer(dataDir)
-	if err != nil {
-		log.Println("rec만들기 에러", err)
-	}
+
+	server.samples = make([]face.Descriptor, 0, 1000000)
+	server.cats = make([]int32, 0, 1000000)
+	server.identify = make(map[int32]identifypt)
 
 	server.m = new(sync.Mutex)
+
+	server.respool = sync.Pool{
+		New: func() interface{} { return new(pb.Res) },
+	}
+	server.ffpool = sync.Pool{
+		New: func() interface{} { return [128]float32{} },
+	}
+	server.facestructpool = sync.Pool{
+		New: func() interface{} { return new(facestruct) },
+	}
+	server.recpool = sync.Pool{
+		New: func() interface{} {
+			return func() *face.Recognizer {
+				rec, _ := face.NewRecognizer(dataDir)
+				return rec
+			}
+		},
+	}
 
 	return server
 }
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) Recog(ctx context.Context, in *pb.Face) (*pb.Res, error) {
-	var ff [128]float32
-	var res *pb.Res
+
+	ff := s.ffpool.Get().([128]float32)
+	res := s.respool.Get().(*pb.Res)
+	rec := s.recpool.Get().(*face.Recognizer)
+	s.facestruct = s.facestructpool.Get().(*facestruct)
 
 	if len(in.Jumin) > 0 { //주민번호가 있을때-얼굴 저장
 		for i, f := range in.Descriptor_ {
@@ -69,25 +99,35 @@ func (s *server) Recog(ctx context.Context, in *pb.Face) (*pb.Res, error) {
 		}
 		s.m.Lock()
 
-		s.samples = append(s.samples, face.Descriptor(ff)) //전송받은 얼굴특징값을 face.Descriptor로 변환하여 samples에 넣기
-		s.jumins = append(s.jumins, in.Jumin)
-		s.names = append(s.names, in.Name)
+		s.facestruct.samples = append(s.facestruct.samples, face.Descriptor(ff)) //전송받은 얼굴특징값을 face.Descriptor로 변환하여 samples에 넣기
+		s.facestruct.cat = int32(len(s.facestruct.samples) - 1)
+		s.facestruct.cats = append(s.facestruct.cats, s.facestruct.cat)
+		s.facestruct.identify[s.facestruct.cat] = identifypt{jumin: in.Jumin, name: in.Name}
+
+		rec.SetSamples(s.facestruct.samples, s.cats)
+
 		s.m.Unlock()
-		res = &pb.Res{Jumin: "", Name: ""}
+
+		res.Jumin = ""
+		res.Name = ""
+		ff = [128]float32{}
+
+		s.ffpool.Put(ff)
+		s.respool.Put(res)
+		s.recpool.Put(rec)
+		s.facestructpool.Put(s.facestruct)
 
 	} else { //주민번호 없을때-얼굴 구분
 		for i, f := range in.Descriptor_ {
 			ff[i] = f
 		}
-		n := f.Compare(s.samples, face.Descriptor(ff), 0.6)
+
+		n := rec.Classify(face.Descriptor(ff))
+
 		if n == -1 {
-			res = &pb.Res{Jumin: "없는얼굴", Name: "없는얼굴"}
-			//log.Printf("Received: %v", in.Face)
+			res = &pb.Res{Jumin: "X", Name: "X"}
 		} else {
-			res = &pb.Res{Jumin: s.jumins[n], Name: s.names[n]}
-			//log.Printf("Received: %v", in.Face)
-			//log.Printf("Received name:%v,jumin:%v,r:%v", res.Name, res.Jumin, n)
-			//log.Printf("보낸주민:%v|보낸이름:%v\n", res.Jumin, res.Name)
+			res = &pb.Res{Jumin: s.facestruct.identify[int32(n)].jumin, Name: s.facestruct.identify[int32(n)].name}
 		}
 	}
 	//fmt.Println(len(samples))
